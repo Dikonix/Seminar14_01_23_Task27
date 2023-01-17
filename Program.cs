8531534e06896c54892e65904fffd80eea853e86
Diana Brik <di.brik@yandex.kz>
2023-01-17 22:18:03 +0600

    Added procedure

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int accumulator = number;
-int sum = 0;
-
-while (accumulator > 0)
+﻿void Sum(int number)
 {
-    sum += accumulator % 10;
-    accumulator /= 10;
+    int accumulator = number;
+    int sum = 0;
+
+    while (accumulator > 0)
+    {
+        sum += accumulator % 10;
+        accumulator /= 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
 }
-Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Sum(number);
