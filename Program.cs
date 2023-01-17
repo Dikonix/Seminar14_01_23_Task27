072c4759e262136731e0c16941b51ee836916bb8
Diana Brik <di.brik@yandex.kz>
2023-01-17 21:48:42 +0600

    Seminar 14.01.23 Task 27

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int accumulator = number;
+int sum = 0;
+
+while (accumulator > 0)
+{
+    sum += accumulator % 10;
+    accumulator /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
